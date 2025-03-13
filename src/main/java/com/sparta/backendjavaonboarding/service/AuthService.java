ded93212824d479ad983586ae019b5eb6ceecf9e
gebeen-dev <rrdd1207@gmail.com>
2025-03-13 16:20:40 +0900

    feat : AuthService 생성

---
+++ src/main/java/com/sparta/backendjavaonboarding/service/AuthService.java
@@ -0,0 +1,11 @@
+package com.sparta.backendjavaonboarding.service;
+
+import com.sparta.backendjavaonboarding.dto.request.SignupRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+	public void singup(SignupRequest reqDto) {
+
+	}
+}
