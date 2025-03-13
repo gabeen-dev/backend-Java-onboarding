888bcb49d86277ae1d551492b864bba767b7d933
gebeen-dev <rrdd1207@gmail.com>
2025-03-13 12:44:55 +0900

    initial commit

---
+++ src/test/java/com/sparta/backendjavaonboarding/BackendJavaOnboardingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.backendjavaonboarding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendJavaOnboardingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
