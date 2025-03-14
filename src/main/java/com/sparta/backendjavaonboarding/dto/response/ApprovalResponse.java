6496147e1ba3a58c00453071250c81c746b93493
gebeen-dev <rrdd1207@gmail.com>
2025-03-14 13:00:20 +0300

    feat : ApprovalResponse 필드 추가

--- src/main/java/com/sparta/backendjavaonboarding/dto/response/ApprovalResponse.java
+++ src/main/java/com/sparta/backendjavaonboarding/dto/response/ApprovalResponse.java
@@ -1,4 +1,13 @@
 package com.sparta.backendjavaonboarding.dto.response;
 
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
 public class ApprovalResponse {
+
+	private String username;
+	private String nickname;
+	private List<RoleResponse> roles;
 }
