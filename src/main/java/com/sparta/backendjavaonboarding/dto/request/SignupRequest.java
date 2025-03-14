25e87dbb2b45c8032eef508becad46dab3d90780
gebeen-dev <rrdd1207@gmail.com>
2025-03-14 12:59:23 +0300

    feat : SignupDto 필드 추가

--- src/main/java/com/sparta/backendjavaonboarding/dto/request/SignupRequest.java
+++ src/main/java/com/sparta/backendjavaonboarding/dto/request/SignupRequest.java
@@ -1,4 +1,12 @@
 package com.sparta.backendjavaonboarding.dto.request;
 
+import lombok.Getter;
+
+@Getter
 public class SignupRequest {
+
+	private String username;
+	private String password;
+	private String nickname;
+
 }
--- src/main/java/com/sparta/backendjavaonboarding/dto/response/SignupResponse.java
+++ src/main/java/com/sparta/backendjavaonboarding/dto/response/SignupResponse.java
@@ -1,4 +1,13 @@
 package com.sparta.backendjavaonboarding.dto.response;
 
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
 public class SignupResponse {
+
+	private String username;
+	private String nickname;
+	private List<RoleResponse> roles;
 }
