e8a432181f2ad0d695617e5b0f8928b16d451f20
gebeen-dev <rrdd1207@gmail.com>
2025-03-14 13:03:20 +0300

    feat : ErrorResponse 생성 및 필드 추가

---
+++ src/main/java/com/sparta/backendjavaonboarding/dto/response/ErrorResponse.java
@@ -0,0 +1,17 @@
+package com.sparta.backendjavaonboarding.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ErrorResponse {
+	private ErrorDetail error;
+
+	@Getter
+	@AllArgsConstructor
+	public static class ErrorDetail {
+		private String code;
+		private String message;
+	}
+}
