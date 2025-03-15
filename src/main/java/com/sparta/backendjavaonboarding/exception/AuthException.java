cb9ee13cc09dabda055ed34f8d9cead2389a8414
gebeen-dev <rrdd1207@gmail.com>
2025-03-15 17:12:10 +0900

    feat : AuthException 생성 및 구현

---
+++ src/main/java/com/sparta/backendjavaonboarding/exception/AuthException.java
@@ -0,0 +1,19 @@
+package com.sparta.backendjavaonboarding.exception;
+
+import lombok.Getter;
+
+@Getter
+public class AuthException extends RuntimeException {
+
+	private ExceptionCode exceptionCode;
+
+	public AuthException(String message, ExceptionCode exceptionCode) {
+		super(message);
+		this.exceptionCode = exceptionCode;
+	}
+
+	public AuthException(String message) {
+		super(message);
+	}
+
+}
