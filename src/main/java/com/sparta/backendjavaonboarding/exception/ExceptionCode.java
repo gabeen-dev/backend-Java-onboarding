5f9d0d2e2614a8a7f8e1ee42a3ff38b9bd1b555e
gebeen-dev <rrdd1207@gmail.com>
2025-03-15 17:13:38 +0900

    feat : ExceptionCode enum 생성및 필드값 추가

---
+++ src/main/java/com/sparta/backendjavaonboarding/exception/ExceptionCode.java
@@ -0,0 +1,17 @@
+package com.sparta.backendjavaonboarding.exception;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@RequiredArgsConstructor
+public enum ExceptionCode {
+
+	USER_ALREADY_EXISTS(HttpStatus.BAD_REQUEST, "이미 가입된 사용자입니다."),
+	INVALID_CREDENTIALS(HttpStatus.BAD_REQUEST, "아이디 또는 비밀번호가 올바르지 않습니다."),
+	INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 인증 토큰입니다."),
+	ACCESS_DENIED(HttpStatus.UNAUTHORIZED, "접근 권한이 없습니다.");
+
+	private final HttpStatus status;
+	private final String message;
+
+}
